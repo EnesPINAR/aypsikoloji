2decf9891605cfd64ae00c8fa704d26ec9684024
enesp <enespinar2@icloud.com>
2024-11-28 22:19:13 +0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
  /** @type {import('tailwindcss').Config} */
  module.exports = {
-     content: ["./templates/**/*.html", "./**/templates/**/*.html", "./node_modules/flyonui/dist/js/*.js"],
+     content: ["./templates/**/*.html", "./**/templates/**/*.html"],
      darkMode: "media",
      theme: {
          extend: {},
      },
      plugins: [
-        require("flyonui"),
-        require("flyonui/plugin")
-    ]
+    ],
  };
