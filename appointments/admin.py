9c3a6b4fcad0a93577ad5785f6a2fe22910d66f4
enesp <enespinar2@icloud.com>
2025-09-10 19:21:29 +0300

    update verbose name of appointment model

--- appointments/admin.py
+++ appointments/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from appointments.models import Psychologist, WorkingSlot, Appointment
 # Register your models here.
 
-
 admin.site.register(Psychologist)
 admin.site.register(WorkingSlot)
 admin.site.register(Appointment)
