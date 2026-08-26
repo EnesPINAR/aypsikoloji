3e884c331908d324953664a962b33161469df3c1
EnesPINAR <63356265+EnesPINAR@users.noreply.github.com>
2026-08-26 21:55:04 +0300

    feat: Gmail SMTP e-posta gonderim altyapisi entegre edildi

--- aypsikoloji/settings.py
+++ aypsikoloji/settings.py
@@ -147,3 +147,13 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
 ]
+
+# E-posta (Gmail SMTP) Konfigürasyonu
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'bildirim.aypsikoloji@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'traokisavilzwjif')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'AyPsikoloji <bildirim.aypsikoloji@gmail.com>')
+
